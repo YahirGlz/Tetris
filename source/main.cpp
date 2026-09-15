95f0b27e3d372fa2e257d60db49aac97bfedc36f
Yahir González <gp.yahir08@gmail.com>
2026-09-14 20:46:22 -0600

    Prueba

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@ int main(){
     cout << "FIRTS COMMIT" << endl;
     cout << "TEXT" << endl;
     cout << "CAMBIO EN GITHUB" << endl;
+    cout << "PEQUEÑA PRUEBA" << endl;
     return 0;
 }   
     
